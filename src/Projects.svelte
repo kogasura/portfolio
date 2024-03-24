89b673d238d9a26f2e8adf6c90f7670cb908a483
yuuki_okubo <yuuki1118sss@gmail.com>
2024-03-24 18:57:58 +0900

    create svelte files

---
+++ src/Projects.svelte
@@ -0,0 +1,52 @@
+<script>
+    let projects = [
+        {
+            title: "Strategik INC",
+            description: "The awesome guys at Strategik INC were superb to work with because they knew their goals exactly.",
+            imageUrl: "./img/work-strategik.jpg",
+            altText: "Strategik INC",
+            category: "Web Design"
+        },
+        {
+            title: "Ēostre UI Kit",
+            description: "Ēostre UI Kit is made following the latest design trends with focus on usability and fast workflow.",
+            imageUrl: "./img/work-eostre.jpg",
+            altText: "Ēostre UI Kit",
+            category: "Web Design"
+        },
+        {
+            title: "Go Outside",
+            description: "Quick concept for a fictitious wildlife exploration app. Grab the PSD and start your own exploration.",
+            imageUrl: "./img/work-go-outside.jpg",
+            altText: "Go Outside App",
+            category: "App Design"
+        }
+    ];
+</script>
+
+<section id="work" class="work">
+    <h2 class="work__heading title">Latest Projects</h2>
+    <div class="work__wrapper wrapper--large">
+        <ul class="work__list">
+            {#each projects as {title, description, imageUrl, altText, category}, index}
+                <li class="work__list-el {index === 0 ? 'work__list-el--is-active' : ''}">
+                    <div class="work__content">
+                        <div class="work__content-blurb blurb blurb--framed">
+                            <h3 class="blurb__heading">{title}<br>{category}</h3>
+                            <p class="blurb__copy">{description}</p>
+                        </div>
+                        <a class="transform-link" href="#0">See full project</a>
+                    </div>
+                    <div class="work__visual">
+                        <img src="{imageUrl}" alt="{altText}">
+                    </div>
+                </li>
+            {/each}
+        </ul>
+        <ul class="work__navigation">
+            {#each projects as _, index}
+                <li class="work__navigation-el {index === 0 ? 'work__navigation-el--is-active' : ''}"></li>
+            {/each}
+        </ul>
+    </div>
+</section>
---
+++ src/Skill.svelte
@@ -0,0 +1,35 @@
+<!-- このコンポーネントは現在使用していない。templeteファイルにあったので、一応切り分けているだけ。 -->
+<section class="about">
+    <div class="about__wrapper wrapper--small">
+        <div class="about__content">
+            <div class="about__content-blurb blurb blurb--framed">
+                <h2 class="blurb__heading">Nothing is<br>impossible</h2>
+                <p class="blurb__copy no-margin">If you just work hard enough at it</p>
+            </div>
+            <p class="about__content-signature">James Martin</p>
+        </div>
+        <img class="about__visual" src="assets/img/about-visual.png" alt="James Martin">
+    </div>
+</section>
+
+<section class="app-design">
+    <div class="app-design__wrapper wrapper--large">
+        <div class="app-design__content blurb">
+            <h2 class="blurb__heading">Topaz App <br>Design</h2>
+            <p class="blurb__copy blurb__copy--tight">App design for Topaz, an awesome creative company from New York.</p>
+            <a class="transform-link" href="#0">Check it out</a>
+        </div>
+    </div>
+    <figure class="app-design__visual"></figure>
+</section>
+
+<section class="web-design">
+    <div class="web-design__wrapper wrapper--small">
+        <div class="web-design__content blurb">
+            <h2 class="blurb__heading">MGF Web <br>Design</h2>
+            <p class="blurb__copy blurb__copy--tight">Web design I made for MGF, an amazing architecture agency from London.</p>
+            <a class="transform-link" href="#0">Check it out</a>
+        </div>
+    </div>
+    <figure class="web-design__visual"></figure>
+</section>
